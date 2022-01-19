5e528f492f2f2d291e5b2ceccbfe29eed9c14bd5
ntshszl <73737521+ntshszl@users.noreply.github.com>
2022-01-19 11:08:14 +0800

    Add files via upload

---
+++ January.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" MasterPageFile="~/Site.Master" CodeBehind="January.aspx.cs" Inherits="GIT_.January" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+        <div id="content-wrapper" class="d-flex flex-column">
+            <!-- Main Content -->
+            <div id="content" class="mt-5 ml-5 mr-5">
+                <h4>
+                    <center>
+                        January
+                    </center>
+                </h4>
+                <hr />
+                <h6>Choose a file with .CSV format</h6>
+                <div class="container">
+                    <div class="section-title">
+                        <h2>Import Excel as New Record</h2>
+                        <p>Add new information by uploading file below. The file should follow the instructions below. </p>
+                    </div>
+                    <div class="card-excelupload">
+                        <p>Please drag/upload file here.</p>
+                        <div class="excel-fileupload">
+                            <asp:FileUpload class="btn bt-primary" ID="FileUploadExcel" runat="server" />
+                            <asp:Button class="btn btn-success" ID="btnUpload" runat="server" OnClick="btnUpload_Click" Text="Upload File"/>
+                            <asp:Label ID="UploadLabel" runat="server" Text=" "></asp:Label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+</asp:Content>
---
+++ January.aspx.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.IO;
+
+namespace GIT_
+{
+    public partial class January : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnUpload_Click(object sender, EventArgs e)
+        {
+            if (FileUploadExcel.HasFile)
+            {
+                try
+
+                {
+                    //Save File that have uploaded
+                    FileUploadExcel.SaveAs(@"C:\Users\BSPT0765\source\repos\GIT_\GIT_\App_Data" + FileUploadExcel.FileName);
+                    string path1 = @"C:\Users\BSPT0765\source\repos\GIT_\GIT_\App_Data" + FileUploadExcel.FileName;
+                    string path = (Server.MapPath("/ExcelFile/" + FileUploadExcel.FileName));
+                    FileUploadExcel.SaveAs(path);
+
+
+
+                    string ExcelConnString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + path1 + ";Extended Properties=Excel 4.0;";
+                    OleDbConnection OleDBcon = new OleDbConnection(ExcelConnString);
+
+                    //Using ole db to read file from excel 
+                    OleDbCommand cmd = new OleDbCommand("Select * from [Sheet1$] ", OleDBcon);
+
+                    OleDbDataAdapter objAdapter1 = new OleDbDataAdapter(cmd);
+                    DataSet ds = new DataSet();
+                    objAdapter1.Fill(ds);
+
+                    OleDBcon.Open();
+
+                    // Create DbDataReader to Data Worksheet  
+                    DbDataReader dr = cmd.ExecuteReader();
+
+                    // SQL Server Connection String  
+                    string ConnStr = "Data Source=HMSB-M1LGPT0698\\SQLEXPRESS;Initial Catalog=git; Persist Security Info=True; User ID=Git;Password=Honda12345";
+
+                    // Bulk Copy to SQL Server
+                    SqlBulkCopy bulkInsert = new SqlBulkCopy(ConnStr);
+
+                    try
+                    {
+                        bulkInsert.DestinationTableName = "janlot";
+
+                        SqlBulkCopyColumnMapping warehouse_ = new SqlBulkCopyColumnMapping("Warehouse", "warehouse");
+                        bulkInsert.ColumnMappings.Add(warehouse_);
+
+                        SqlBulkCopyColumnMapping lotno_ = new SqlBulkCopyColumnMapping("Lot Number", "lotno");
+                        bulkInsert.ColumnMappings.Add(lotno_);
+
+                        SqlBulkCopyColumnMapping country_ = new SqlBulkCopyColumnMapping("Country", "country");
+                        bulkInsert.ColumnMappings.Add(country_);
+
+                        bulkInsert.WriteToServer(dr);
+                    }
+
+                    catch (Exception labelex)
+                    {
+                        Response.Write("<script>alert('Data Duplicated/Error')</script>");
+                        Response.Write(labelex);
+
+                    }
+                    OleDBcon.Close();
+
+                    Response.Write("<script>alert('Data Inserted Successfully.')</script>");
+                    UploadLabel.Text = FileUploadExcel.FileName + " Uploaded";
+                }
+
+                catch (Exception)
+                {
+                    Response.Write("<script>alert('Error occured. Data may duplicated.')</script>");
+                    UploadLabel.Text = "Error while uploading excel file.";
+                }
+            }
+
+            else
+            {
+                UploadLabel.Text = "No file uploaded";
+            }
+
+        }
+
+    }
+}
